0bae43a4f98ce2ffd38f2a04340c8356840c866b
Peter Gray <peter@petergraycreative.com>
2018-03-23 10:52:28 -0500

    Built Schema, Rewrote login, and Newuser
    Also Wrote user password hash and check password functions to use promises.

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -5,24 +5,38 @@ const Schema = mongoose.Schema;
 const SALT_ROUNDS = 11;
 
 const UserSchema = Schema({
-  // create your user schema here.
-  // username: required, unique and lowercase
-  // password: required
+  username: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-UserSchema.pre('save', function(next) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // if there is an error here you'll need to handle it by calling next(err);
-  // Once the password is encrypted, call next() so that your userController and create a user
-});
+  UserSchema.pre('save', function(next) {
+    bcrypt
+      .hash(this.password, SALT_ROUNDS)
+      .then((hash) => {
+        this.password = hash;
+        next();
+      })
+      .catch((err) => err);
+  });
 
-UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this method in with the Proper password comparing, bcrypt.compare()
-  // Your controller will be responsible for sending the information here for password comparison
-  // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
-};
+  UserSchema.methods.checkPassword = (user, potentialPassword) => {
+    return new Promise((resolve, reject) => {
+      return bcrypt
+        .compare(potentialPassword, user.password)
+        .then((isMatch) => {
+          resolve(isMatch);
+        })
+        .catch((err) => reject(err));
+    }).catch((err) => err);
+  };
 
 // if you're really stuck with this at this point, you can reference this document.
 // https://github.com/LambdaSchool/Auth-JWT/blob/master/models/index.js This is what we're going for here.
